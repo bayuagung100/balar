b90e0ec569313a1bc7183829035126f3622219c9
bayuagung100 <bayuagung100@gmail.com>
2018-03-28 08:09:34 +0700

    Update call.js

--- call.js
+++ call.js
@@ -19,7 +19,17 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-a_to_ve[i].href="http://weightlossblog.online?go="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+var x=Math.floor((Math.random()*9)+ 1);
+var xxx=null;
+if(x=="1"){xxx="/the-following-tips-for-your-fitness/"}
+if(x=="2"){xxx="/simple-ways-to-improve-your-health/"}
+if(x=="3"){xxx="/solid-advice-for-getting-in-great-shape/"}
+if(x=="4"){xxx="/get-fit-with-these-simple-fitness-tips/"}
+if(x=="5"){xxx="/interesting-tips-and-tricks-about-physical-fitness/"}
+if(x=="6"){xxx="/ways-of-getting-in-shape-and-staying-fit/"}
+if(x=="7"){xxx="/real-advice-on-how-to-get-more-fit/"}
+if(x=="8"){xxx="/get-the-body-youve-always-dreamed-of/"}
+if(x=="9"){xxx="/awesome-ideas-to-help-you-trim-some-fat/"}a_to_ve[i].href="http://weightlossblog.online"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
